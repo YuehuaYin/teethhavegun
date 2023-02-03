8aeeb70edcaeb084be4696366c62ad0baa4d3481
Kai Davis <kd2g20@soton.ac.uk>
2023-02-03 22:49:38 +0000

    TIMOTHY MOVES, HE'S ALIVEgit add -A

--- Assets/Scripts/Player/playerMovement.cs
+++ Assets/Scripts/Player/playerMovement.cs
@@ -5,10 +5,13 @@ using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
+    Vector2 moveD;
+    Rigidbody2D rb;
+    public int speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,9 +19,13 @@ public class playerMovement : MonoBehaviour
     {
         
     }
-
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(speed * moveD.x, rb.velocity.y);
+    }
     public void OnMove(InputAction.CallbackContext context)
     {
-        Debug.Log("hello");
+        Debug.Log(context.ReadValue<Vector2>());
+        moveD = new Vector2(context.ReadValue<Vector2>().x, 0);
     }
 }
